6bf0365cbbaa0df41e01bb43a167cf870443498c
Ewan Jee <830jiforcareer@gmail.com>
2024-02-20 19:33:29 -0500

    feat/admin NoArgsConstructor 추가

--- src/main/java/com/newjob/consultant/entity/Admin.java
+++ src/main/java/com/newjob/consultant/entity/Admin.java
@@ -1,11 +1,14 @@
 package com.newjob.consultant.entity;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "newjob_admin")
 public class Admin {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
