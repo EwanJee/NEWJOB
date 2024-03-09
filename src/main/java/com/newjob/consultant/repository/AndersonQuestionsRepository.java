c14d59ab06c51cf70bff9a82a5f58b4c889e121c
Ewan Jee <830jiforcareer@gmail.com>
2024-03-09 18:56:51 -0500

    feat(AndersonQuestions) JpaRepository 추가

---
+++ src/main/java/com/newjob/consultant/repository/AndersonQuestionsRepository.java
@@ -0,0 +1,7 @@
+package com.newjob.consultant.repository;
+
+import com.newjob.consultant.entity.AndersonQuestion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AndersonQuestionsRepository extends JpaRepository<AndersonQuestion, Long> {
+}
