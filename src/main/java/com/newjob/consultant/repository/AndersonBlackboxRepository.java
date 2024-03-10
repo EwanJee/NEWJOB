0f0529e649f0c91df646c97c54373a134704ddcf
Ewan Jee <830jiforcareer@gmail.com>
2024-03-10 17:51:34 -0400

    feat/repository AndersonBlackboxRepository 추가

---
+++ src/main/java/com/newjob/consultant/repository/AndersonBlackboxRepository.java
@@ -0,0 +1,7 @@
+package com.newjob.consultant.repository;
+
+import com.newjob.consultant.entity.AndersonBlackbox;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AndersonBlackboxRepository extends JpaRepository<AndersonBlackbox, Integer> {
+}
