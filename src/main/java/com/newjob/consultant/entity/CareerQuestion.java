7170d4b1ca22248963f4d733c1b813ab2937cb30
Ewan Jee <830jiforcareer@gmail.com>
2024-03-13 18:18:24 -0400

    feat/entity CareerQuestion 접근 제한자 추가

--- src/main/java/com/newjob/consultant/entity/CareerQuestion.java
+++ src/main/java/com/newjob/consultant/entity/CareerQuestion.java
@@ -4,10 +4,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Table(name = "career_questions")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 public class CareerQuestion {
     @Id
