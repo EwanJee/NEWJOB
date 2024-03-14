7de57f988ad919ffa1630d6fea03bb5cef08861a
Ewan Jee <830jiforcareer@gmail.com>
2024-03-14 00:56:02 -0400

    feat/test admin test1 작성

--- src/test/java/com/newjob/consultant/admin/AdminTests.java
+++ src/test/java/com/newjob/consultant/admin/AdminTests.java
@@ -2,19 +2,29 @@ package com.newjob.consultant.admin;
 
 import com.newjob.consultant.entity.Admin;
 import com.newjob.consultant.repository.AdminRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-
 @DataJpaTest
+@ExtendWith(MockitoExtension.class)
 public class AdminTests {
     @Autowired
     private AdminRepository adminRepository;
+    @Test
+    @DisplayName("어드민 레포지토리에서 첫번째 인덱스와 id가 1인 어드민의 id가 같은지 확인")
     public void test1(){
         //Given
-        Admin admin = adminRepository.find
+        Admin admin = adminRepository.findAll().get(0);
 
         //When
+        Admin admin1 = adminRepository.findById(1L).orElse(null);
 
         //Then
+        assert admin1 != null;
+        Assertions.assertEquals(admin.getId(), admin1.getId());
     }
 }
