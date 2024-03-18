a25bb51a99d5cd75da4b198db685056e31b49fbf
Ewan Jee <830jiforcareer@gmail.com>
2024-03-18 16:58:25 -0400

    add/ErrorCode enum

--- src/main/java/com/newjob/consultant/common/exception/ErrorCode.java
+++ src/main/java/com/newjob/consultant/common/exception/ErrorCode.java
@@ -1,5 +1,12 @@
 package com.newjob.consultant.common.exception;
 
-public enum ErrorCode {
+import lombok.AllArgsConstructor;
 
+@AllArgsConstructor
+public enum ErrorCode {
+    NOT_FOUND(404, "COMMON-ERR-404", "PAGE NOT FOUND");
+
+    private int status;
+    private String errorCode;
+    private String message;
 }
