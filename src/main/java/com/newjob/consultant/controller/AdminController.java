ce80b7e88c0041f1d40c8829452e9c48bb83a27e
Ewan Jee <830jiforcareer@gmail.com>
2024-03-18 22:15:17 -0400

    add/AdminController 에러 처리 메서드 생성

--- src/main/java/com/newjob/consultant/controller/AdminController.java
+++ src/main/java/com/newjob/consultant/controller/AdminController.java
@@ -1,49 +1,54 @@
 package com.newjob.consultant.controller;
 
+import com.newjob.consultant.common.exception.NotFoundException;
 import com.newjob.consultant.entity.Consultant;
 import com.newjob.consultant.service.AdminService;
 import com.newjob.consultant.service.ConsultantService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
 public class AdminController {
     private final AdminService adminService;
-    private final ConsultantService consultantService ;
+    private final ConsultantService consultantService;
+
+    @ExceptionHandler(NotFoundException.class)
+    public String catchException(Exception error, Model model){
+        model.addAttribute("error",error);
+        return "exception/error";
+    }
 
     @GetMapping("/admin")
-    public String admin(Model model){
-        model.addAttribute("admin",new AdminForm());
+    public String admin(Model model) {
+        model.addAttribute("admin", new AdminForm());
         return "admin/adminLogin";
     }
+
     @PostMapping("/admin")
-    public String isAdmin(@ModelAttribute("admin") AdminForm adminForm, Model model){
-        if(adminService.isAdmin(adminForm.getId())) {
+    public String isAdmin(@ModelAttribute("admin") AdminForm adminForm, Model model) {
+        if (adminService.isAdmin(adminForm.getId())) {
             List<Consultant> consultantList = consultantService.findAll();
-            model.addAttribute("consultantList",consultantList);
+            model.addAttribute("consultantList", consultantList);
             return "admin/consultantList";
-        }
-        else{
+        } else {
             return "redirect:/admin";
         }
     }
+
     @GetMapping("/admin/list")
-    public String consultantList(Model model){
+    public String consultantList(Model model) {
         List<Consultant> consultantList = consultantService.findAll();
-        model.addAttribute("consultantList",consultantList);
+        model.addAttribute("consultantList", consultantList);
         return "admin/consultantList";
     }
+
     @GetMapping("/admin/list/{id}/edit")
-    public String editConsultant(@PathVariable("id")Long id, Model model){
+    public String editConsultant(@PathVariable("id") Long id, Model model) {
         Consultant consultant = consultantService.findById(id);
         ConsultantForm consultantForm = new ConsultantForm();
         consultantForm.setId(consultant.getId());
@@ -57,16 +62,18 @@ public class AdminController {
         consultantForm.setNumberOfUsedMrAndersonTests(consultant.getNumberOfUsedMrAndersonTests());
         consultantForm.setIsApproved(consultant.getIsApproved());
 
-        model.addAttribute("consultant",consultantForm);
+        model.addAttribute("consultant", consultantForm);
         return "admin/updateConsultantForm";
     }
+
     @PostMapping("/admin/list/{id}/edit")
-    public String editedConsultant(@PathVariable("id")Long id, @ModelAttribute("consultantForm") ConsultantForm consultantForm){
-        adminService.updateForm(id,consultantForm.getNumberOfAvailableMrAndersonTests(),consultantForm.getNumberOfAvailableCareerTests(),consultantForm.getIsApproved());
+    public String editedConsultant(@PathVariable("id") Long id, @ModelAttribute("consultantForm") ConsultantForm consultantForm) {
+        adminService.updateForm(id, consultantForm.getNumberOfAvailableMrAndersonTests(), consultantForm.getNumberOfAvailableCareerTests(), consultantForm.getIsApproved());
         return "redirect:/admin/list";
     }
+
     @GetMapping("/admin/list/{id}/delete")
-    public String deletedConsultant(@PathVariable("id")Long id){
+    public String deletedConsultant(@PathVariable("id") Long id) {
         adminService.deleteForm(id);
         return "redirect:/admin/list";
     }
