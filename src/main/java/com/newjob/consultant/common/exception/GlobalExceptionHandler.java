41db7e8b586ebf872d6f182a8ffd14d5e8f22ded
Ewan Jee <830jiforcareer@gmail.com>
2024-03-24 18:15:15 -0400

    add/GlobalExceptionHandler,NotFoundException & edit/ErrorCode

---
+++ src/main/java/com/newjob/consultant/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.newjob.consultant.common.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@Slf4j
+@ControllerAdvice
+public class GlobalExceptionHandler extends RuntimeException {
+    @ExceptionHandler(NotFoundException.class)
+    public String NotFoundException(NotFoundException ex) {
+        log.warn(ex.getMessage());
+        return "error/not_found.html";
+    }
+
+}
