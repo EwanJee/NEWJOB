9db81225968bf78d3676e27e81f48756b0c84ef9
Ewan Jee <830jiforcareer@gmail.com>
2024-03-24 19:43:46 -0400

    edit/GlobalExceptionHandler ModelAndView적용

--- src/main/java/com/newjob/consultant/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/newjob/consultant/common/exception/GlobalExceptionHandler.java
@@ -1,16 +1,22 @@
 package com.newjob.consultant.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @ControllerAdvice
-public class GlobalExceptionHandler extends RuntimeException {
+public class GlobalExceptionHandler{
     @ExceptionHandler(NotFoundException.class)
-    public String NotFoundException(NotFoundException ex) {
+    public ModelAndView NotFoundException(NotFoundException ex) {
         log.warn(ex.getErrorCode().getMessage());
-        return "error/not_found.html";
+//        model.addAttribute("error",ex);
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("ex",ex.getErrorCode());
+        mav.setViewName("/error/not_found");
+        return mav;
     }
 
 }
