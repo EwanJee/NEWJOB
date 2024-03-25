d7a6481ba67ae011d7b99c3755bb0307325f5f64
Ewan Jee <830jiforcareer@gmail.com>
2024-03-24 22:06:12 -0400

    edit/ErrorCode 에러코드 변경 AND not_found.html 수정

--- src/main/java/com/newjob/consultant/common/exception/ErrorCode.java
+++ src/main/java/com/newjob/consultant/common/exception/ErrorCode.java
@@ -6,10 +6,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
-    CONSULTANT_NOT_FOUND(404, "CONSULTANT-ERR-404", "해당 컨설턴트를 찾을 수 없습니다"),
-    CAREER_TEST_RESULT_NOT_FOUND(404,"CareerTestResult-ERR-404","해당 커리어 진단 리포트 결과를 찾을 수 없습니다"),
-    ANDERSON_TEST_RESULT_NOT_FOUND(404,"CareerTestResult-ERR-404","해당 퇴직 진단 리포트 결과를 찾을 수 없습니다"),
-    LOGIN_FAILED(404, "LOGIN-ERR-404","비밀번호나 로그인이 잘못 되었습니다");
+    CONSULTANT_NOT_FOUND(404, "컨설턴트 정보 불일치", "해당 컨설턴트를 찾을 수 없습니다"),
+    CAREER_TEST_RESULT_NOT_FOUND(404,"커리어 진단 리포트 정보 불일치","해당 커리어 진단 리포트 결과를 찾을 수 없습니다"),
+    ANDERSON_TEST_RESULT_NOT_FOUND(404,"퇴직 진단 리포트 정보 불일치","해당 퇴직 진단 리포트 결과를 찾을 수 없습니다"),
+    LOGIN_FAILED(404, "회원 정보 불일치","비밀번호나 로그인이 잘못 되었습니다");
 
     private final int status;
     private final String errorCode;
