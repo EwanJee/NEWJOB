7f9fe1087b944459c194486b5184c8f22809a50f
Ewan Jee <830jiforcareer@gmail.com>
2024-03-28 18:26:00 -0400

    edit nothing

--- src/test/java/com/newjob/consultant/controller/AdminControllerTest.java
+++ src/test/java/com/newjob/consultant/controller/AdminControllerTest.java
@@ -6,23 +6,24 @@ import com.newjob.consultant.repository.ConsultantRepository;
 import com.newjob.consultant.repository.MrAndersonTestResultRepository;
 import com.newjob.consultant.service.AdminService;
 import com.newjob.consultant.service.ConsultantService;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
-
+@ExtendWith(MockitoExtension.class)
 @WebMvcTest(AdminController.class)
 class AdminControllerTest {
-    private final AdminRepository adminRepository = mock(AdminRepository.class);
-    private final ConsultantRepository consultantRepository = mock(ConsultantRepository.class);
-    private final AdminService adminService = new AdminService(adminRepository,consultantRepository);
-    private final CareerTestResultRepository careerTestResultRepository = mock(CareerTestResultRepository.class);
-    private final MrAndersonTestResultRepository mrAndersonTestResultRepository = mock(MrAndersonTestResultRepository.class);
-    private final ConsultantService consultantService = new ConsultantService(consultantRepository,careerTestResultRepository,mrAndersonTestResultRepository);
+    private AdminRepository adminRepository = mock(AdminRepository.class);
+    private ConsultantRepository consultantRepository = mock(ConsultantRepository.class);
+    private AdminService adminService = new AdminService(adminRepository,consultantRepository);
+    private CareerTestResultRepository careerTestResultRepository = mock(CareerTestResultRepository.class);
+    private MrAndersonTestResultRepository mrAndersonTestResultRepository = mock(MrAndersonTestResultRepository.class);
+    private ConsultantService consultantService = new ConsultantService(consultantRepository,careerTestResultRepository,mrAndersonTestResultRepository);
 
     private MockMvc mockMvc;
     @Test
