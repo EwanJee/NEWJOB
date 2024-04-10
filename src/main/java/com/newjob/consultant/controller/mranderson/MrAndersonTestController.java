7ef3ebaec731ae7a27ea2e167e848d88ed682a0d
Ewan Jee <830jiforcareer@gmail.com>
2024-04-10 18:17:53 -0400

    edit / Controller 클래스 안 updateScore 사용하도록 변경

--- src/main/java/com/newjob/consultant/controller/mranderson/MrAndersonTestController.java
+++ src/main/java/com/newjob/consultant/controller/mranderson/MrAndersonTestController.java
@@ -76,7 +76,7 @@ public class MrAndersonTestController {
 
     @PostMapping("/{id}/test/mrAnderson/{testId}/test2")
     public String andersonTest2(@PathVariable("id") Long id, @PathVariable("testId") Long testId, @ModelAttribute("form") MrAndersonTestResultForm mrAndersonTestResultForm, Model model) {
-        mrAndersonTestService.updateScore1(testId, mrAndersonTestResultForm.getScore1(), mrAndersonTestResultForm.getScore2(), mrAndersonTestResultForm.getScore3(), mrAndersonTestResultForm.getScore4(), mrAndersonTestResultForm.getScore5(), mrAndersonTestResultForm.getScore6(), mrAndersonTestResultForm.getScore7(), mrAndersonTestResultForm.getScore8());
+        mrAndersonTestService.updateScore(testId, mrAndersonTestResultForm.getScore1(), mrAndersonTestResultForm.getScore2(), mrAndersonTestResultForm.getScore3(), mrAndersonTestResultForm.getScore4(), mrAndersonTestResultForm.getScore5(), mrAndersonTestResultForm.getScore6(), mrAndersonTestResultForm.getScore7(), mrAndersonTestResultForm.getScore8());
         model.addAttribute("id", id);
         model.addAttribute("testId", testId);
         model.addAttribute("form", mrAndersonTestResultForm);
@@ -86,7 +86,7 @@ public class MrAndersonTestController {
 
     @PostMapping("/{id}/test/mrAnderson/{testId}/test3")
     public String andersonTest3(@PathVariable("id") Long id, @PathVariable("testId") Long testId, @ModelAttribute("form") MrAndersonTestResultForm mrAndersonTestResultForm, Model model) {
-        mrAndersonTestService.updateScore2(testId,mrAndersonTestResultForm.getScore9(),mrAndersonTestResultForm.getScore10(),mrAndersonTestResultForm.getScore11(),mrAndersonTestResultForm.getScore12(),mrAndersonTestResultForm.getScore13(),mrAndersonTestResultForm.getScore14(),mrAndersonTestResultForm.getScore15(),mrAndersonTestResultForm.getScore16());
+        mrAndersonTestService.updateScore(testId,mrAndersonTestResultForm.getScore9(),mrAndersonTestResultForm.getScore10(),mrAndersonTestResultForm.getScore11(),mrAndersonTestResultForm.getScore12(),mrAndersonTestResultForm.getScore13(),mrAndersonTestResultForm.getScore14(),mrAndersonTestResultForm.getScore15(),mrAndersonTestResultForm.getScore16());
         model.addAttribute("id", id);
         model.addAttribute("testId", testId);
         model.addAttribute("form", mrAndersonTestResultForm);
@@ -95,7 +95,7 @@ public class MrAndersonTestController {
     }
     @PostMapping("/{id}/test/mrAnderson/{testId}/test4")
     public String andersonTest4(@PathVariable("id") Long id, @PathVariable("testId") Long testId, @ModelAttribute("form") MrAndersonTestResultForm mrAndersonTestResultForm, Model model) {
-        mrAndersonTestService.updateScore3(testId,mrAndersonTestResultForm.getScore17(),mrAndersonTestResultForm.getScore18(),mrAndersonTestResultForm.getScore19(),mrAndersonTestResultForm.getScore20(),mrAndersonTestResultForm.getScore21(),mrAndersonTestResultForm.getScore22(),mrAndersonTestResultForm.getScore23(),mrAndersonTestResultForm.getScore24());
+        mrAndersonTestService.updateScore(testId,mrAndersonTestResultForm.getScore17(),mrAndersonTestResultForm.getScore18(),mrAndersonTestResultForm.getScore19(),mrAndersonTestResultForm.getScore20(),mrAndersonTestResultForm.getScore21(),mrAndersonTestResultForm.getScore22(),mrAndersonTestResultForm.getScore23(),mrAndersonTestResultForm.getScore24());
         model.addAttribute("id", id);
         model.addAttribute("testId", testId);
         model.addAttribute("form", mrAndersonTestResultForm);
@@ -104,7 +104,7 @@ public class MrAndersonTestController {
     }
     @PostMapping("/{id}/test/mrAnderson/{testId}/test5")
     public String andersonTest5(@PathVariable("id") Long id, @PathVariable("testId") Long testId, @ModelAttribute("form") MrAndersonTestResultForm mrAndersonTestResultForm, Model model) {
-        mrAndersonTestService.updateScore4(testId,mrAndersonTestResultForm.getScore25(),mrAndersonTestResultForm.getScore26(),mrAndersonTestResultForm.getScore27(),mrAndersonTestResultForm.getScore28(),mrAndersonTestResultForm.getScore29(),mrAndersonTestResultForm.getScore30(),mrAndersonTestResultForm.getScore31(),mrAndersonTestResultForm.getScore32());
+        mrAndersonTestService.updateScore(testId,mrAndersonTestResultForm.getScore25(),mrAndersonTestResultForm.getScore26(),mrAndersonTestResultForm.getScore27(),mrAndersonTestResultForm.getScore28(),mrAndersonTestResultForm.getScore29(),mrAndersonTestResultForm.getScore30(),mrAndersonTestResultForm.getScore31(),mrAndersonTestResultForm.getScore32());
         model.addAttribute("id", id);
         model.addAttribute("testId", testId);
         model.addAttribute("form", mrAndersonTestResultForm);
@@ -113,7 +113,7 @@ public class MrAndersonTestController {
     }
     @PostMapping("/{id}/test/mrAnderson/{testId}/test6")
     public String andersonTest6(@PathVariable("id") Long id, @PathVariable("testId") Long testId, @ModelAttribute("form") MrAndersonTestResultForm mrAndersonTestResultForm, Model model) {
-        mrAndersonTestService.updateScore5(testId,mrAndersonTestResultForm.getScore33(),mrAndersonTestResultForm.getScore34(),mrAndersonTestResultForm.getScore35(),mrAndersonTestResultForm.getScore36(),mrAndersonTestResultForm.getScore37(),mrAndersonTestResultForm.getScore38(),mrAndersonTestResultForm.getScore39(),mrAndersonTestResultForm.getScore40());
+        mrAndersonTestService.updateScore(testId,mrAndersonTestResultForm.getScore33(),mrAndersonTestResultForm.getScore34(),mrAndersonTestResultForm.getScore35(),mrAndersonTestResultForm.getScore36(),mrAndersonTestResultForm.getScore37(),mrAndersonTestResultForm.getScore38(),mrAndersonTestResultForm.getScore39(),mrAndersonTestResultForm.getScore40());
         model.addAttribute("id", id);
         model.addAttribute("testId", testId);
         model.addAttribute("form", mrAndersonTestResultForm);
@@ -122,7 +122,7 @@ public class MrAndersonTestController {
     }
     @PostMapping("/{id}/test/mrAnderson/{testId}/result")
     public String andersonResult(@PathVariable("id") Long id, @PathVariable("testId") Long testId, @ModelAttribute("form") MrAndersonTestResultForm mrAndersonTestResultForm, Model model){
-        mrAndersonTestService.updateScore6(testId,mrAndersonTestResultForm.getScore41(),mrAndersonTestResultForm.getScore42(),mrAndersonTestResultForm.getScore43(),mrAndersonTestResultForm.getScore44(),mrAndersonTestResultForm.getScore45(),mrAndersonTestResultForm.getScore46(),mrAndersonTestResultForm.getScore47(),mrAndersonTestResultForm.getScore48());
+        mrAndersonTestService.updateScore(testId,mrAndersonTestResultForm.getScore41(),mrAndersonTestResultForm.getScore42(),mrAndersonTestResultForm.getScore43(),mrAndersonTestResultForm.getScore44(),mrAndersonTestResultForm.getScore45(),mrAndersonTestResultForm.getScore46(),mrAndersonTestResultForm.getScore47(),mrAndersonTestResultForm.getScore48());
         consultantService.updateNumberOfUsedMrAndersonTests(id);
         consultantService.addMrAndersonTest(id,testId);
         return "redirect:/consultant/" + id + "/test/mrAnderson/finish/" + testId;
