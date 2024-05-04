f8f48b10d677562edafcaaa785d3ab425a5c2212
Ewan Jee <830jiforcareer@gmail.com>
2024-05-04 15:37:49 -0400

    add / NPE 에러 핸들링 추가

--- src/main/java/com/newjob/consultant/common/exception/ErrorCode.java
+++ src/main/java/com/newjob/consultant/common/exception/ErrorCode.java
@@ -1,5 +1,17 @@
 package com.newjob.consultant.common.exception;
 
-public enum ErrorCode {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+    CONSULTANT_NOT_FOUND(404,"컨설턴트 정보 불일치","해당 컨설턴트를 찾을 수 없습니다"),
+    CAREER_TEST_RESULT_NOT_FOUND(404, "커리어 진단 리포트 정보 불일치","해당 커리어 진단 리포트를 찾을 수 없습니다"),
+    ANDERSON_TEST_RESULT_NOT_FOUND(404, "퇴직 진단 리포트 정보 불일치","해당 퇴직 진단 리포트를 찾을 수 없습니다"),
+    LOGIN_FAILED(404, "회원 정보 불일치","비밀번호나 로그인이 잘못 되었습니다");
+
+    private final int status;
+    private final String errorCode;
+    private final String message;
 }
