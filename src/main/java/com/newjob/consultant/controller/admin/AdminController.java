aa040d600de1183b386667d12b63ebb8518173c0
Ewan Jee <830jiforcareer@gmail.com>
2024-05-26 13:37:11 +0900

    edit / requestMapping 적용

--- src/main/java/com/newjob/consultant/controller/admin/AdminController.java
+++ src/main/java/com/newjob/consultant/controller/admin/AdminController.java
@@ -7,24 +7,22 @@ import com.newjob.consultant.service.consultant.ConsultantService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @AllArgsConstructor
+@RequestMapping("/admin")
 public class AdminController {
     private final AdminService adminService;
     private final ConsultantService consultantService;
 
-    @GetMapping("/admin")
+    @GetMapping
     public String admin(Model model) {
         model.addAttribute("admin", new AdminForm());
         return "admin/adminLogin";
     }
 
-    @PostMapping("/admin")
+    @PostMapping
     public String isAdmin(@ModelAttribute("admin") AdminForm adminForm, Model model) {
         if (adminService.isAdmin(adminForm.getId())) {
             model.addAttribute("consultantList", consultantService.findAll());
@@ -34,25 +32,25 @@ public class AdminController {
         }
     }
 
-    @GetMapping("/admin/list")
+    @GetMapping("/list")
     public String consultantList(Model model) {
         model.addAttribute("consultantList", consultantService.findAll());
         return "admin/consultantList";
     }
 
-    @GetMapping("/admin/list/{id}/edit")
+    @GetMapping("/list/{id}/edit")
     public String editConsultant(@PathVariable("id") Long id, Model model) {
         model.addAttribute("consultant", consultantService.findById(id));
         return "admin/updateConsultantForm";
     }
 
-    @PostMapping("/admin/list/{id}/edit")
+    @PostMapping("/list/{id}/edit")
     public String editedConsultant(@PathVariable("id") Long id, @ModelAttribute("consultantForm") ConsultantForm consultantForm) {
         consultantService.updateForm(id, consultantForm);
         return "redirect:/admin/list";
     }
 
-    @GetMapping("/admin/list/{id}/delete")
+    @GetMapping("/list/{id}/delete")
     public String deletedConsultant(@PathVariable("id") Long id) {
         consultantService.deleteForm(id);
         return "redirect:/admin/list";
